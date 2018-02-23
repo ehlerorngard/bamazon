8d87edf9ab0d9b8672b7572c3bc7feb58163ccc1
ehler orngard <ehlerorngard@ehlers-MacBook.local>
2018-02-23 00:26:24 -0800

    functions mostly fleshed out

---
+++ bamazon.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+	item_id INT(30) AUTO_INCREMENT NOT NULL,
+	product_name VARCHAR(90) NOT NULL,
+	department_name VARCHAR(30),
+	price DECIMAL(10, 2),
+	stock_quantity INT(6),
+	PRIMARY KEY(item_id)
+);
+
+
